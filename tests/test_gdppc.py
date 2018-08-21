bc343823cf9bb2d9f77d38a2793ea016e89f5b63
James Rising <jarising@gmail.com>
2018-08-21 02:43:23 -0700

    misyears

--- tests/test_gdppc.py
+++ tests/test_gdppc.py
@@ -10,15 +10,15 @@ def test_zwe_low():
     helper(provider_low, 'ZWE.2.2', 188.74704269, 569.97540343, 576.72664619)
 
 def test_zwe_high():
-    helper(provider_high, 'ZWE.2.2', 607.12035717, 1846.70740714, 1929.81909533)
+    helper(provider_high, 'ZWE.2.2', 607.12035717, 2618.03101405, 2731.7025934)
 
 # ABW exists only in the high IAM: check that the same
     
 def test_abw_low():
-    helper(provider_low, 'ABW', 1411.21495327, 4268.54105942, 4391.36859299)
+    helper(provider_low, 'ABW', 1411.21495327, 5180.55555556, 5321.58780557)
 
 def test_abw_high():
-    helper(provider_high, 'ABW', 1411.21495327, 4268.54105942, 4391.36859299)
+    helper(provider_high, 'ABW', 1411.21495327, 5180.55555556, 5321.58780557)
 
 # XYZ is not a country; check that starts same and grows differently
     
@@ -32,7 +32,6 @@ def helper(provider, region, in2010, in2050, in2051):
     assert provider.get_startyear() <= 2010
     series = provider.get_timeseries(region)
     assert series[2010 - provider.get_startyear()] == pytest.approx(in2010)
-    print region, series[2050 - provider.get_startyear()], in2050
     assert series[2050 - provider.get_startyear()] == pytest.approx(in2050)
     assert series[2051 - provider.get_startyear()] == pytest.approx(in2051)
 
