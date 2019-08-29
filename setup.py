24ab9cb37fc60a6d3aed88a2b7964bcce8e121da
Brewster Malevich <bmalevich@rhg.com>
2019-08-29 15:02:59 -0700

    Fix package setup

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='impactcommon',
       version='0.1',
       description='Library of shared tools across the impact team.',
       url='https://github.com/ClimateImpactLab/impact-common',
       author='Impacts Team',
-      packages=['impactcommon'],
+      packages=find_packages(),
       license='GNU v. 3',
       long_description=open('README.md').read(),
-)
+      long_description_content_type='text/markdown',
+      install_requires=['numpy', 'pandas', 'xarray', 'metacsv', 'openest',
+                        'impactlab-tools'],
+      tests_require=['pytest'],
+      )
