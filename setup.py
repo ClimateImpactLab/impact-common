44533d74b4c0c423adc741102971d47ccf39f818
Brewster Malevich <bmalevich@rhg.com>
2020-08-06 10:37:48 -0700

    Swap in setuptools_scm to handle versioning
    This will make it easier to diagnose bugs for users with 'dev' installs.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='impactcommon',
-      version='0.1',
+      use_scm_version=True,
       description='Library of shared tools across the impact team.',
       url='https://github.com/ClimateImpactLab/impact-common',
       author='Impacts Team',
@@ -9,6 +9,7 @@ setup(name='impactcommon',
       license='GNU v. 3',
       long_description=open('README.md').read(),
       long_description_content_type='text/markdown',
+      setup_requires=['setuptools_scm'],
       install_requires=['numpy', 'pandas', 'xarray', 'metacsv', 'openest',
                         'impactlab-tools'],
       extras_require={
