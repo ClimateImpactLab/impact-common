5caecd209809a2edffa1ca466699ff0dc129a273
Emile Tenezakis <etenezakis@battuta.berkeley.edu>
2021-10-04 13:33:49 -0700

    Revert "compiled math functions for runnign stats"
    This reverts commit c06f5a3d62d61ba3b2648e15461f38ca9b8836b7.

--- impactcommon/math/averages.py
+++ impactcommon/math/averages.py
@@ -1,37 +1,4 @@
 import numpy as np
-import numba as nb
-
-@nb.njit 
-def compiled_npmean(x):
-    return np.mean(x).item()
-
-@nb.njit 
-def compiled_npmedian(x):
-    return np.median(x).item()
-
-@nb.njit 
-def compiled_sum(x):
-    return float(sum(x))
-    
-@nb.njit 
-def compiled_npflipud(x):
-    return np.flipud(x)
-
-@nb.njit
-def compiled_bucket_updater(curlen, length, sumval, value):
-    if curlen >= length:
-        sumval = (length - 1) * sumval / curlen + value
-        if curlen > length:
-            curlen = length
-    else:
-        sumval += value
-        curlen += 1
-
-    return curlen, length, sumval
-
-@nb.njit
-def compiled_npdot(x,y):
-    return np.dot(x,y)
 
 class RunningStatistic(object):
     """
@@ -56,10 +23,10 @@ class MemoryAverager(RunningStatistic):
         super(MemoryAverager, self).__init__(length)
 
         if len(values) >= length:
-            self.values = np.array(values[-length:], dtype=np.float64)
+            self.values = np.array(values[-length:])
             self.write_index = 0 # overwrite at this location next time
         else:
-            self.values = np.array(values, dtype=np.float64)
+            self.values = list(values) # Keep as a list
             self.write_index = None # append next time
 
     def update(self, value):
@@ -67,8 +34,9 @@ class MemoryAverager(RunningStatistic):
             self.values[self.write_index] = value
             self.write_index = (self.write_index + 1) % self.length
         else:
-            self.values = np.append(self.values, value) 
+            self.values.append(value)
             if len(self.values) == self.length:
+                self.values = np.array(self.values)
                 self.write_index = 0
 
 class MeanAverager(MemoryAverager):
@@ -76,14 +44,14 @@ class MeanAverager(MemoryAverager):
     Simple mean running average.
     """
     def get(self):
-        return compiled_npmean(self.values)
+        return np.mean(self.values).item()
 
 class MedianAverager(MemoryAverager):
     """
     Simple median running average.
     """
     def get(self):
-        return compiled_npmedian(self.values)
+        return np.median(self.values).item()
 
 class BucketAverager(RunningStatistic):
     """
@@ -91,14 +59,22 @@ class BucketAverager(RunningStatistic):
     """
     def __init__(self, values, length):
         super(BucketAverager, self).__init__(length)
-        self.sumval = compiled_sum(values)
+        self.sumval = float(sum(values))
         self.curlen = len(values)
 
     def update(self, value):
-        self.curlen, self.length, self.sumval = compiled_bucket_updater(self.curlen, self.length, self.sumval, value)
+        #assert self.curlen <= self.length, "{0} > {1}".format(self.curlen, self.length)
+
+        if self.curlen >= self.length:
+            self.sumval = (self.length - 1) * self.sumval / self.curlen + value
+            if self.curlen > self.length:
+                self.curlen = self.length
+        else:
+            self.sumval += value
+            self.curlen += 1
 
     def get(self):
-        return self.sumval/self.curlen 
+        return self.sumval / self.curlen
 
 class KernelAverager(MemoryAverager):
     """
@@ -106,27 +82,28 @@ class KernelAverager(MemoryAverager):
     """
     def __init__(self, values, kernel):
         super(KernelAverager, self).__init__(values, len(kernel))
-        self.kernel = compiled_npflipud(np.array(kernel) / compiled_sum(kernel))
+        self.kernel = np.flipud(np.array(kernel) / sum(kernel))
     
     def get(self):
         if self.write_index is None or self.write_index == 0:
             subkernel = self.kernel[-len(self.values):]
-            out = compiled_npdot(subkernel, self.values) / compiled_sum(subkernel)
+            out = np.dot(subkernel, self.values) / np.sum(subkernel)
         else:
             recentkernel = self.kernel[-self.write_index:]
             olderkernel = self.kernel[:-self.write_index]
-            out = compiled_npdot(recentkernel, self.values[:self.write_index]) + compiled_npdot(olderkernel, self.values[self.write_index:])
-        return out
+            out = np.dot(recentkernel, self.values[:self.write_index]) + np.dot(olderkernel, self.values[self.write_index:])
+        return out.item()
 
     def get_calculation(self):
         if self.write_index is None or self.write_index == 0:
             subkernel = self.kernel[-len(self.values):]
-            return ' + '.join(["{0} * {1}".format(subkernel[ii] / compiled_sum(subkernel), self.values[ii]) for ii in range(len(subkernel))])
+            return ' + '.join(["{0} * {1}".format(subkernel[ii] / np.sum(subkernel), self.values[ii]) for ii in range(len(subkernel))])
         else:
             recentkernel = self.kernel[-self.write_index:]
             olderkernel = self.kernel[:-self.write_index]
             return ' + '.join(["{0} * {1}".format(recentkernel[ii], self.values[ii]) for ii in range(len(recentkernel))]) + ' + '.join(["{0} * {1}".format(olderkernel[ii], self.values[self.write_index + ii]) for ii in range(len(olderkernel))])
         
+
 class KernelMeanAverager(KernelAverager):
     """
     Kernel-based implementation of a simple running average.
@@ -139,7 +116,7 @@ class BartlettAverager(KernelAverager):
     Bartlett running average.
     """
     def __init__(self, values, length=5):
-        super(BartlettAverager, self).__init__(values, compiled_npflipud(np.arange(length) + 1.))
+        super(BartlettAverager, self).__init__(values, np.flipud(np.arange(length) + 1.))
 
 def translate(cls, length, data):
     avg = cls([], length)
@@ -153,12 +130,12 @@ def translate(cls, length, data):
 if __name__ == '__main__':
     for cls in [MeanAverager, MedianAverager, BucketAverager, KernelMeanAverager, BartlettAverager]:
         print(cls)
-        avg = cls(np.array([0,1,2,3]), 5)
+        avg = cls(list(range(4)), 5)
         print(avg.get(), (0 + 1 + 2 + 3) / 4.)
         avg.update(4)
         print(avg.get(), (0 + 1 + 2 + 3 + 4) / 5.)
         avg.update(5)
-        print(avg.get(), (0 + 1 + 2 + 3 + 4 + 5) / 5.)
+        print(avg.get(), (1 + 2 + 3 + 4 + 5) / 5.)
 
     clses = [MeanAverager, MedianAverager, BucketAverager, KernelMeanAverager, BartlettAverager]
     averages = [cls(np.zeros(25), 30) for cls in clses]
